657841bc95c97b481f25aa1c9e75f4e45cd73655
dsilvador <dev.maquinads@gmail.com>
2021-06-27 20:21:51 -0300

    Adiciona slide de notícias

---
+++ assets/js/carrossel.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper-container', {
+    slidesPerView: 3.2,
+    spaceBetween: 20,
+        // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
