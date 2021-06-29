eb3b0debb60122238f49f4ad7c8b51be9380d278
dsilvador <dev.maquinads@gmail.com>
2021-06-29 12:30:47 -0300

    Ajustes

--- assets/mail/contact_me.php
+++ assets/mail/contact_me.php
@@ -1,6 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['message']) || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
   http_response_code(500);
   exit();
 }
