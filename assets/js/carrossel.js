83a8f67ee513329b35254c00141fc056bda2029b
dsilvador <dev.maquinads@gmail.com>
2021-06-29 11:51:38 -0300

    Ajustes mobile

--- assets/js/carrossel.js
+++ assets/js/carrossel.js
@@ -1,10 +1,31 @@
 const swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3.2,
-    spaceBetween: 20,
-        // Navigation arrows
+    
+    breakpoints: {
+        640: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2.2,
+            spaceBetween: 40,
+        },
+        1366: {
+            slidesPerView: 3.2,
+            spaceBetween: 20,
+        },
+        1920: {
+            slidesPerView: 4.2,
+            spaceBetween: 20,
+        }
+    },
+
+     // Navigation arrows
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+        el: '.swiper-pagination',
     },
 
 });
